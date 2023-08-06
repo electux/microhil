6e49cbe6aa8f68c99ced756ad1834d7e872810de
Vladimir Roncevic <elektron.ronca@gmail.com>
2023-08-06 12:45:17 +0200

    [microhil] Added skeleton for log

---
+++ sw/microhildesk/log/microhil_log_abstract.h
@@ -0,0 +1,38 @@
+/* -*- Mode: CC; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * microhil_log_abstract.h
+ * Copyright (C) 2023 Vladimir Roncevic <elektron.ronca@gmail.com>
+ *
+ * microhildesk is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * microhildesk is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <glibmm.h>
+
+enum class LogLevel
+{
+    DEBUG = 0,
+    INFO = 1,
+    WARNING = 2,
+    ERROR = 3
+};
+
+class AbMicroHILLog
+{
+    public:
+        virtual ~AbMicroHILLog() = default;
+
+        virtual void write(Glib::ustring message, LogLevel level) = 0;
+};
